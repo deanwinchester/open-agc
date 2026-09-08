ae9a3388e26dbec038eb68d9c3c7a7611a6e2bfd
deanwinchester <deanwinchester@163.com>
2026-09-08 14:16:12 +0800

    fix: 玲珑 sources 改为工程内相对路径解包（ll-builder 1.5.x 不支持 file:// 协议）

--- scripts/build_linglong.sh
+++ scripts/build_linglong.sh
@@ -75,8 +75,6 @@ sed -e "s|@APP_ID@|${APP_ID}|g" \
     -e "s|@APP_NAME@|${APP_NAME}|g" \
     -e "s|@LL_VERSION@|${LL_VERSION}|g" \
     -e "s|@LL_BASE@|${LL_BASE}|g" \
-    -e "s|@DEB_FILE@|file://${PWD}/${PROJECT_DIR}/open-agc.deb|g" \
-    -e "s|@DEB_DIGEST@|$(sha256sum "${PROJECT_DIR}/open-agc.deb" | cut -d' ' -f1)|g" \
     packaging/linglong.yaml.template > "${PROJECT_DIR}/linglong.yaml"
 
 # ---- 3. Build ----
